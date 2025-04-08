a10143f33a720c60f53e5240552797a183ec5439
guilherme0066 <cursioalves@gmail.com>
2025-04-08 13:50:53 -0300

    ex pronto

---
+++ src/test/java/br/gov/sp/lab4/exemplo/ExemploApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.sp.lab4.exemplo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
